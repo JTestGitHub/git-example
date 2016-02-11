fbcc02b6c555132e4d39f85257690fdd98cb014b
mjoesome <mjoesome@mail.com>
2016-02-11 11:48:53 -0700

    First commit

---
+++ SampleClone/SampleClone/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace SampleClone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnComment = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnComment
+            // 
+            this.btnComment.Location = new System.Drawing.Point(108, 94);
+            this.btnComment.Name = "btnComment";
+            this.btnComment.Size = new System.Drawing.Size(100, 23);
+            this.btnComment.TabIndex = 0;
+            this.btnComment.Text = "Get A Comment";
+            this.btnComment.UseVisualStyleBackColor = true;
+            this.btnComment.Click += new System.EventHandler(this.btnComment_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(329, 237);
+            this.Controls.Add(this.btnComment);
+            this.Name = "Form1";
+            this.Text = "Random Programming Comments";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnComment;
+    }
+}
+
---
+++ SampleClone/SampleClone/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SampleClone
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> aList = new List<string>();
+        StreamReader sr;
+        Random rand = new Random();
+        public void AdviseMe()
+        {
+            sr = File.OpenText("Comments.txt");
+            while (!sr.EndOfStream)
+            {
+                aList.Add(sr.ReadLine());
+            }
+        }
+
+        private void btnComment_Click(object sender, EventArgs e)
+        {
+            AdviseMe();
+            MessageBox.Show(aList[rand.Next(0, aList.Count() - 1)].ToString());
+        }
+    }
+}
