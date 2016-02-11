0511d3ea333fa3676a443e23688316838adcff5f
mjoesome <mjoesome@mail.com>
2016-02-11 13:07:57 -0700

    second commit

--- SampleClone/SampleClone/Form1.cs
+++ SampleClone/SampleClone/Form1.cs
@@ -18,15 +18,21 @@ namespace SampleClone
             InitializeComponent();
         }
         List<string> aList = new List<string>();
-        StreamReader sr;
+       
         Random rand = new Random();
         public void AdviseMe()
         {
-            sr = File.OpenText("Comments.txt");
-            while (!sr.EndOfStream)
-            {
-                aList.Add(sr.ReadLine());
-            }
+            aList.Add("Hello world never taught me programming");
+            aList.Add("Breakpoints don't break a program");
+            aList.Add("Nothing programmed nothing displayed");
+            aList.Add("I don't know how the Internet code got into my program, honest");
+            aList.Add("A comment on how comments are helpful is a helpful comment");
+            aList.Add("Agile programming doesn't mean your program can dance");
+            aList.Add("SQL is not short for sequel even though you say that way");
+            aList.Add("Clean code is dry clean only");
+            aList.Add("Programming events are not the same as a life events");
+            aList.Add("The programmer lifecycle: Test, Program, Get paid, Repeat");
+            aList.Add("Programming is hard, I just make it look easy");
         }
 
         private void btnComment_Click(object sender, EventArgs e)
